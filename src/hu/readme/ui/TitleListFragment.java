3a603b5b4964cba8e1c5f41e799b383e49ffda90
Csaba Golubov <csaba.golubov@bitknights.com>
2014-10-09 21:53:43 +0200

    Minor work on title list

---
+++ src/hu/readme/ui/TitleListFragment.java
@@ -0,0 +1,11 @@
+package hu.readme.ui;
+
+import android.app.Fragment;
+
+public class TitleListFragment extends Fragment {
+
+	public TitleListFragment() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
